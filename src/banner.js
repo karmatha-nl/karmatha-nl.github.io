caa3caae0d94cbad19e066dc3095bba698449567
Wouter van den Heuvel <exo_00@hotmail.com>
2017-09-11 23:58:21 +0200

    Added render listener to banner

--- src/banner.js
+++ src/banner.js
@@ -1,4 +1,9 @@
 
 $(document).ready(function() {    
-	var s = skrollr.init();
+	skrollr.init({
+		render: function(data) {
+			//Log the current scroll position.
+			console.log(data.curTop);
+		}
+	});
 });
