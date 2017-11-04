a310bbc472141c3422b8e874944b37273a2a185d
Wouter van den Heuvel <exo_00@hotmail.com>
2017-11-04 18:23:51 +0100

    Feature detection instead of browser detectoin

--- src/hero.js
+++ src/hero.js
@@ -20,6 +20,8 @@ function Hero(container) {
 	this.idleTime = 0;
 
 	this.spritesheet = null;
+	this.fps = 30;
+	this.lastUpdate = performance.now();
 
 	this.video = null;
 	this.videoChapters = {
@@ -30,7 +32,68 @@ function Hero(container) {
 		SCRATCH2: { start: 30, end: 32 }
 	}
 
-	this.setMode('spritesheet');	
+	this.init();
+	this.update = this.update.bind(this);
+}
+
+Hero.prototype.init = function() {
+	// Check if we can support alpha WebM video 
+	supportsWebMAlpha(function(useVideo){
+		this.setMode(useVideo ? 'video' : 'spritesheet');		
+	}.bind(this));
+
+	function supportsWebMAlpha(callback){
+		var vid = document.createElement('video');
+		vid.autoplay = false;
+		vid.loop = false;
+		vid.style.display = "none";
+		vid.addEventListener("loadeddata", function(){
+			document.body.removeChild(vid);
+			// Create a canvas element, this is what user sees.
+			var canvas = document.createElement("canvas");
+	
+			//If we don't support the canvas, we definitely don't support webm alpha video.
+			if (!(canvas.getContext && canvas.getContext('2d'))){
+				callback(false);
+				return;
+			}
+	
+			// Get the drawing context for canvas.
+			var ctx = canvas.getContext("2d");
+	
+			// Draw the current frame of video onto canvas.
+			ctx.drawImage(vid, 0, 0);
+			callback(ctx.getImageData(0, 0, 1, 1).data[3] === 0);
+	
+		}, false);
+		vid.addEventListener("error", function(){
+			document.body.removeChild(vid);
+			callback(false);
+		});
+	
+		vid.addEventListener("stalled", function(){
+			document.body.removeChild(vid);
+			callback(false);
+		});
+	
+		//Just in case
+		vid.addEventListener("abort", function(){
+			document.body.removeChild(vid);
+			callback(false);
+		});
+	
+		var source = document.createElement("source");
+		source.src="data:video/webm;base64,GkXfowEAAAAAAAAfQoaBAUL3gQFC8oEEQvOBCEKChHdlYm1Ch4ECQoWBAhhTgGcBAAAAAAACBRFNm3RALE27i1OrhBVJqWZTrIHlTbuMU6uEFlSua1OsggEjTbuMU6uEHFO7a1OsggHo7AEAAAAAAACqAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAVSalmAQAAAAAAADIq17GDD0JATYCNTGF2ZjU3LjU3LjEwMFdBjUxhdmY1Ny41Ny4xMDBEiYhARAAAAAAAABZUrmsBAAAAAAAARq4BAAAAAAAAPdeBAXPFgQGcgQAitZyDdW5khoVWX1ZQOYOBASPjg4QCYloA4AEAAAAAAAARsIFAuoFAmoECU8CBAVSygQQfQ7Z1AQAAAAAAAGfngQCgAQAAAAAAAFuhooEAAACCSYNCAAPwA/YAOCQcGFQAADBgAABnP///NXgndmB1oQEAAAAAAAAtpgEAAAAAAAAk7oEBpZ+CSYNCAAPwA/YAOCQcGFQAADBgAABnP///Vttk7swAHFO7awEAAAAAAAARu4+zgQC3iveBAfGCAXXwgQM=";
+		source.addEventListener("error", function()
+		{
+		   document.body.removeChild(vid);
+		   callback(false);
+		});
+		vid.appendChild(source);
+	
+		//This is required for IE
+		document.body.appendChild(vid);
+	};	
 }
 
 Hero.prototype.playFragment = function(fragment) {
@@ -79,6 +142,7 @@ Hero.prototype.setMode = function(mode) {
 		}	
 	}
 	else if (mode === 'spritesheet') {
+		
 		if(!this.spritesheet){
 			this.container.append("<div id='hero-spritesheet' style='background-image: url(\"/resources/sequence-spritesheet.png\")'></div>");
 			this.spritesheet = document.getElementById('hero-spritesheet');			
@@ -86,8 +150,10 @@ Hero.prototype.setMode = function(mode) {
 		this.spritesheet.style.display = 'block';
 		if(this.video) {
 			this.video.style.display = 'none';
-		}	
-	}
+		}
+		
+		this.update();		
+	}	
 }
 
 Hero.prototype.onVideoUpdate = function () {
@@ -135,7 +201,23 @@ Hero.prototype.onClick = function(e) {
 	
 	return false;
 }
+this.fps = 30;
+this.lastUpdate = Date.now();
+// Spritesheet update 
+Hero.prototype.update = function() { 
+	if(this.mode !== 'spritesheet'){
+		return;
+	}
+	var interval = 1000/this.fps;
+	var now = performance.now();
+	var	delta = now - this.lastUpdate;
+	if (delta > interval) {
+		this.lastUpdate = now - (delta % interval);
 
+	}
+	requestAnimationFrame(this.update);
+	
+}
 
 $(document).ready(function() { 
 	// Assign random icon to appear
@@ -153,16 +235,16 @@ $(document).ready(function() {
 	});
 	skrollr.menu.init(s);
 
-	// This horrible thing is needed because browser don't support webm video with transparency equally
-	var ua = detect.parse(navigator.userAgent);
-	switch (ua.browser.family) {
-		case 'Firefox':
-			if(ua.browser.version < 53) {
-				// Firefox supports transparent video after version 53
-				break;
-			}
-		case 'Chrome':
-			hero.setMode('video');
-		}
+	// // This horrible thing is needed because browser don't support webm video with transparency equally
+	// var ua = detect.parse(navigator.userAgent);
+	// switch (ua.browser.family) {
+	// 	case 'Firefox':
+	// 		if(ua.browser.version < 53) {
+	// 			// Firefox supports transparent video after version 53
+	// 			break;
+	// 		}
+	// 	case 'Chrome':
+	// 		hero.setMode('video');
+	// 	}
 });
 
