bec3958ad3d8414e03e32fa0f84cf97edab64ef7
Wouter van den Heuvel <exo_00@hotmail.com>
2017-11-08 19:11:09 +0100

    Enabled Ajax on form

--- src/form.js
+++ src/form.js
@@ -6,26 +6,26 @@ $(document).ready(function() {
 	$('#contactForm')[0].reset()
 	$('#contactForm').find(':submit').removeAttr('disabled');
 	
-	// $('#contactForm').submit(function(e) {
-	// 	e.preventDefault();
-	// 	$(this).find('.fields').slideUp(500);
-	// 	$(this).find(':submit').val('').attr('disabled', 'disabled');
+	$('#contactForm').submit(function(e) {
+		e.preventDefault();
+		$(this).find('.fields').slideUp(500);
+		$(this).find(':submit').val('').attr('disabled', 'disabled');
 
-	// 	$.ajax({
-	// 		url: this.action,
-	// 		method: 'POST',
-	// 		data: $(this).serialize(),
-	// 		dataType: 'json',
-	// 		beforeSend: function() {
-	// 		},
-	// 		success: function(data) {
-	// 			$(this).find('.fields').promise().done(function(){
-	// 				console.log($('#contactForm #thanks'));
-	// 				$('#contactForm #thanks').show().slideDown(500);
-	// 			});
-	// 		},
-	// 		error: function(err) {
-	// 		}
-	// 	});
-	// });
+		$.ajax({
+			url: this.action,
+			method: 'POST',
+			data: $(this).serialize(),
+			dataType: 'json',
+			beforeSend: function() {
+			},
+			success: function(data) {
+				$(this).find('.fields').promise().done(function(){
+					console.log($('#contactForm #thanks'));
+					$('#contactForm #thanks').show().slideDown(500);
+				});
+			},
+			error: function(err) {
+			}
+		});
+	});
 });
