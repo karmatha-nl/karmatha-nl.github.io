4e5e43facd5fb25604753bca737123d88bd5eb04
Wouter van den Heuvel <exo_00@hotmail.com>
2017-11-08 11:05:00 +0100

    Language is stored in localstorage

--- src/language.js
+++ src/language.js
@@ -5,13 +5,23 @@
 $(document).ready(function() {    
 	// Handle clicking on the language selector menu 
   	$('.lang-selector li').click(function( event ) {
-		$('html').addClass('lang-animate').attr('lang', event.target.getAttribute('lang'));
-    }); 
+		setLanguage(event.target.getAttribute('lang'), true);  
+	}); 
+	
+	function setLanguage(lang, animate) {
+		localStorage.setItem('language', lang);
+		$('html').attr('lang', lang);
+		if(animate){
+			$('html').addClass('lang-animate').attr('lang', lang);					
+		}
+	}
+
+	// Set initial language to browser language
+	var lang = localStorage.getItem("language") || navigator.language.substr(0, 2);
+
+	if(['en', 'nl'].indexOf(lang) > -1){
+		setLanguage(lang);
+	}
 });
 
-// Set initial language to browser language
-var lang = navigator.language.substr(0, 2);
 
-if(['en', 'nl'].indexOf(lang) > -1){
-	$('html').attr('lang', lang);
-}
