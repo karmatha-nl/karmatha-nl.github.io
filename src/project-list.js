2308dcc458decae290456005f10453c8e3e9da16
Wouter van den Heuvel <exo_00@hotmail.com>
2021-05-07 19:38:09 +0200

    project list carousel is responsive

--- src/project-list.js
+++ src/project-list.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
-	$('#projectlist').slick({
-			dots: true,
-			slide: 'a',
-			// slidesToShow: 3,
-			prevArrow: '<div class="nav-arrow"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
-			nextArrow: '<div class="nav-arrow"  style="right: 0"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
-	});
+  $('#projectlist').slick({
+    dots: true,
+    slide: 'a',
+    slidesToShow: 3,
+    prevArrow: '<div class="nav-arrow"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+    nextArrow: '<div class="nav-arrow"  style="right: 0"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      }
+    }]
+  });
 });
