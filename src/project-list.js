042f50cb7b36f7c5b03b87d4fe00fe99ed6e1141
Wouter van den Heuvel <exo_00@hotmail.com>
2021-05-07 19:28:24 +0200

    added the dots

--- src/project-list.js
+++ src/project-list.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	$('#projectlist').slick({
+			dots: true,
 			slide: 'a',
+			// slidesToShow: 3,
 			prevArrow: '<div class="nav-arrow"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
 			nextArrow: '<div class="nav-arrow"  style="right: 0"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
 	});
